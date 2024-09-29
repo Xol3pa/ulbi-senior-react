e039d535777250533dab05db17bd728066c34767
xol3pa <vang.pva@mail.ru>
2024-09-29 16:50:18 +0300

    add pageLoader, Loader, NotFoundPage

--- .eslintrc.js
+++ .eslintrc.js
@@ -43,6 +43,7 @@ module.exports = {
         'no-underscore-dangle': 'off',
         'i18next/no-literal-string': ['error', { markupOnly: true }],
         'max-len': ['error', { ignoreComments: true }],
+        'linebreak-style': null,
     },
     globals: {
         __IS_DEV__: true,
